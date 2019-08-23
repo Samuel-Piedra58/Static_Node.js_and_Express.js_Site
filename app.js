d087df634636c2c8b1f8f74c83875a402f2dd96c
Samuel-Piedra58 <samuel58piedra@gmail.xom>
2019-08-22 23:37:47 -0700

    Included Express error handling and refined error handling

--- app.js
+++ app.js
@@ -5,16 +5,36 @@ const project = require("./routes/project.js");
 
 const app = express();
 
-// CONFIG STATIC SERVER
+// CONFIG STATIC SERVER //////////////////////////////
 app.use("/static", express.static("public"));
 
-// CONFIG TEMPLATING ENGINE
+// CONFIG TEMPLATING ENGINE //////////////////////////////
 app.set("views", __dirname + "/views");
 app.set("view engine", "pug");
 
+// CONFIG ROUES //////////////////////////////
 app.use("/", routes);
-app.use("/project", project);
+app.use("/projects", project);
 
+// FORCE 404 ERROR //////////////////////////////
+app.use(function(req, res, next) {
+  const error = new Error("Page Not Found");
+  error.statusCode = 404;
+  next(error);
+});
+
+// ERROR HANDLER MIDDLEWARE //////////////////////////////
+app.use(function(err, req, res, next) {
+  console.error(`${err.statusCode}: ${err.message}`);
+  console.error(`Request URL: ${req.headers.host + req.originalUrl}`);
+  if (err.statusCode === undefined) {
+    err.statusCode = 500;
+    err.message = "Internal Server Error";
+  }
+  res.status(err.statusCode).render("error", { err });
+});
+
+// START SERVER //////////////////////////////
 app.listen(3000, function() {
   console.log("Starting server on localhost:3000");
 });
