c4f1a479146d185c178fafe9647bf3596534ef71
Samuel-Piedra58 <samuel58piedra@gmail.xom>
2019-08-22 23:36:23 -0700

    Updated projects route to handle base project url

--- routes/project.js
+++ routes/project.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const { projects } = require("../data.json");
 
 // CONFIG ROUTES
+router.get("/", function(req, res) {
+  const randomProjectId = Math.floor(Math.random() * projects.length);
+  const randomProjectRoute = `/projects/${randomProjectId}`;
+  res.redirect(randomProjectRoute);
+});
+
 router.get("/:id", function(req, res) {
   const { id } = req.params;
   const displayProject = projects[id];
