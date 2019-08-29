6c39bf04a3b8787dfdfa6bad57383772ef87873f
Samuel-Piedra58 <samuel58piedra@gmail.xom>
2019-08-28 22:46:44 -0700

    Updated projects route

--- routes/project.js
+++ routes/project.js
@@ -9,11 +9,16 @@ router.get("/", function(req, res) {
   res.redirect(randomProjectRoute);
 });
 
-router.get("/:id", function(req, res) {
-  // ADD ADDITIONAL CHECK FOR VALID ":id" PARAMETER
-  // --- if its undefined?
-  // --- does an item in the array equal that parameter?
+router.get("/:id", function(req, res, next) {
   const { id } = req.params;
+  if (isNaN(id) || id === undefined) {
+    // if requested project is not a number or is undefined then call next()
+    return next();
+  } else if (id < 0 || id >= projects.length) {
+    // if requested project is not an available project then call next()
+    return next();
+  }
+
   const displayProject = projects[id];
   res.render("project", { displayProject });
 });
